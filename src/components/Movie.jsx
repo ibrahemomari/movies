703985d618c4268b49c3e5b7103be68fd6e19a41
ibrahemomari <ibrahem0_0omari@LTUCSTUDENT-L096.localdomain>
2021-10-13 14:28:47 +0300

    kader task

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -3,14 +3,13 @@ import React, { useEffect, useState } from "react";
 import { Card, Col, Container, Row } from "react-bootstrap";
 
 function Movie({ movie }) {
-  
-  const [movies, setmovies] = useState({})
+  const [movies, setmovies] = useState({});
 
-  useEffect(()=>{
-    let data=JSON.parse(localStorage.getItem("move"));
+  useEffect(() => {
+    let data = JSON.parse(localStorage.getItem("move"));
     setmovies(data);
-  })
-  
+  });
+
   return (
     <>
       <Container>
@@ -18,34 +17,36 @@ function Movie({ movie }) {
           <Col xs={9}>
             <Container>
               <Row>
-                  
-                <Card key={movies.id}>
-                <Card.Img
-                  src={`https://image.tmdb.org/t/p/w500${movies.backdrop_path}`}
-                  alt="Card image"
-                />
-                <Card.ImgOverlay>
-                  <Card.Title>
-                    <div
-                      className="movie-vote_average"
-                      style={{
-                        border: `2px solid ${
-                            movies.vote_average > 7
-                            ? "green"
-                            : movies.vote_average < 7 && movies.vote_average > 6
-                            ? "yellow"
-                            : "red"
-                        }`,
-                      }}
-                    >
-                      <p>{movies.vote_average}</p>
-                    </div>
-                  </Card.Title>
-                </Card.ImgOverlay>
-                <Card.Footer>
-                  <Card.Text>Last updated 3 mins ago</Card.Text>
-                </Card.Footer>
-              </Card>
+                {movies && (
+                  <Card key={movies.id}>
+                    <Card.Img
+                      src={`https://image.tmdb.org/t/p/w500${movies.backdrop_path}`}
+                      alt="Card image"
+                    />
+                    <Card.ImgOverlay>
+                      <Card.Title>
+                        <div
+                          className="movie-vote_average"
+                          style={{
+                            border: `2px solid ${
+                              movies.vote_average > 7
+                                ? "green"
+                                : movies.vote_average < 7 &&
+                                  movies.vote_average > 6
+                                ? "yellow"
+                                : "red"
+                            }`,
+                          }}
+                        >
+                          <p>{movies.vote_average}</p>
+                        </div>
+                      </Card.Title>
+                    </Card.ImgOverlay>
+                    <Card.Footer>
+                      <Card.Text>Last updated 3 mins ago</Card.Text>
+                    </Card.Footer>
+                  </Card>
+                )}
               </Row>
             </Container>
           </Col>
